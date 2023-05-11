1e6f56b3aa39b9c278caa6163b00519d90db4735
Diego Giuliani <giuliani.diego@gmail.com>
2023-05-11 13:55:17 -0300

    serving files

--- http_server/src/main.rs
+++ http_server/src/main.rs
@@ -3,12 +3,16 @@ use website_handler::WebsiteHandler;
 
 use crate::server::Server;
 
+use std::env;
 mod http;
 mod server;
 mod website_handler;
 
 fn main() {
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
     let server = Server::new("127.0.0.1:8080".to_string());
 
-    server.run(WebsiteHandler {});
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+
+    server.run(WebsiteHandler::new({ public_path }));
 }
--- http_server/src/website_handler.rs
+++ http_server/src/website_handler.rs
@@ -1,13 +1,51 @@
-use crate::{http::Response, server::Handler};
+use crate::{
+    http::{Method, Response, StatusCode},
+    server::Handler,
+};
 
-pub struct WebsiteHandler {}
+use std::fs;
+
+pub struct WebsiteHandler {
+    public_path: String,
+}
+
+impl WebsiteHandler {
+    pub fn new(public_path: String) -> Self {
+        Self { public_path }
+    }
+
+    fn read_file(&self, file_path: &str) -> Option<String> {
+        let path = format!("{}/{}", self.public_path, file_path);
+        match fs::canonicalize(path) {
+            Ok(path) => {
+                if path.starts_with(&self.public_path) {
+                    fs::read_to_string(path).ok()
+                } else {
+                    println!("Directory Traversal Attack attemp");
+                    None
+                }
+            }
+            Err(_) => None,
+        }
+    }
+}
 
 impl Handler for WebsiteHandler {
     fn handle_request(&mut self, request: &crate::http::Request) -> crate::http::Response {
         dbg!(request);
-        Response::new(
-            crate::http::StatusCode::Ok,
-            Some("<h1>Hello world!</h1>".to_string()),
-        )
+        match request.method() {
+            Method::GET => match request.path() {
+                "/" => Response::new(
+                    crate::http::StatusCode::Ok,
+                    self.read_file("index.html"),
+                    // Some("<h1>Home Page!</h1>".to_string()),
+                ),
+                path => match self.read_file(path) {
+                    Some(contents) => Response::new(crate::http::StatusCode::Ok, Some(contents)),
+                    None => Response::new(StatusCode::NotFound, None),
+                },
+            },
+            _ => Response::new(StatusCode::NotFound, None),
+        }
     }
 }
